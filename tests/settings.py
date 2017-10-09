252bdc6ff235f37dcb7cae2f8d9722034256b9ed
Nicholas Wolff <nwolff@gmail.com>
2017-10-09 13:30:36 +0200

    check migrations

--- tests/settings.py
+++ tests/settings.py
@@ -2,11 +2,10 @@
 
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db'
     },
 }
 
